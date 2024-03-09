1cd2dc8aaec984b4fa4ea6709c6e6060fbe3fdf4
ClupTheGreat <playclupfree@gmail.com>
2024-03-09 11:20:43 +0530

    Created start activity

--- app/src/main/java/com/example/parkingfinder/login_activity.java
+++ app/src/main/java/com/example/parkingfinder/login_activity.java
@@ -1,8 +1,6 @@
 package com.example.parkingfinder;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,30 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Splash_Screen extends AppCompatActivity {
-
-    private static int SPLASH_TIME_OUT = 1500;
+public class login_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        new Handler().postDelayed(new Runnable(){
-            @Override
-            public void run() {
-                Intent i = new Intent(Splash_Screen.this, Start_Activity.class);
-                startActivity(i);
-
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
-
     }
 }
